a4eca5af892413e3772aef8395e6a1f2d3bfc8dc
saaanchez08 <92090087+saaanchez08@users.noreply.github.com>
2024-04-18 19:53:36 +0200

    [ADD] DTO

--- coca-tiendaProyecto/src/main/java/com/coca/tienda/dtos/CategoriaDTO.java
+++ coca-tiendaProyecto/src/main/java/com/coca/tienda/dtos/CategoriaDTO.java
@@ -1,5 +1,39 @@
 package com.coca.tienda.dtos;
 
 public class CategoriaDTO {
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+
+	public CategoriaDTO(Integer id, String nombre, String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
 }
