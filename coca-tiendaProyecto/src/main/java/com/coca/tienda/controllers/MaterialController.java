06fdfd1bc197878cbfdc3209f5e0b83f55414807
Sergio <sergiocp99@gmail.com>
2024-06-12 15:56:04 +0200

    [ADD] Material REST 2.0

--- coca-tiendaProyecto/src/main/java/com/coca/tienda/controllers/MaterialController.java
+++ coca-tiendaProyecto/src/main/java/com/coca/tienda/controllers/MaterialController.java
@@ -3,12 +3,14 @@ package com.coca.tienda.controllers;
 import com.coca.tienda.dtos.MaterialDTO;
 import com.coca.tienda.negocio.impl.MaterialService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/tienda/material")
 public class MaterialController {
